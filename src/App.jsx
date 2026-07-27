c54c41c872f187542117c4ad1cd6f93b7ba58e8e
Temesgen Meharie <155353727+temesgenmeharie@users.noreply.github.com>
2026-07-27 09:44:46 +0300

    refactor: remove unused CRA files, duplicate subfolder, and fix ESLint rules

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./index.css";
 
-
-
- 
 export default function App() {
   return (
     <div className="scroll-smooth">
